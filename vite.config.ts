e5237357cf2e82fdf18aec151dfac08280403492
Isabel Vieira <work.isabelvieira@gmail.com>
2023-11-30 18:01:52 -0700

    fix: added base url for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  ...(isProd ? { base: '/digimon-table/' } : {}),
 });
